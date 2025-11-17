03a27a492a84dc285024d23858407c4e7e0b9835
ArLaN <arlanmaximov1409@gmail.com>
2025-11-17 19:20:31 +0500

    updated comments
    Simple code for simple telegram bot , with some features and buttons!

--- code_bot.py
+++ code_bot.py
@@ -1,17 +1,17 @@
 @bot.message_handler(commands=['cmd'])
 def send_keys(message):
     keyboard = ReplyKeyboardMarkup()
-    keyboard.add(KeyboardButton('кнопка 1'), KeyboardButton('кнопка 2'), KeyboardButton('sup'), KeyboardButton('what are you?'))
-    bot.send_message(message.chat.id, 'привет!', reply_markup=keyboard)
+    keyboard.add(KeyboardButton('button 1'), KeyboardButton('button 2'), KeyboardButton('sup'), KeyboardButton('what are you?'))
+    bot.send_message(message.chat.id, 'Hello , buttons created!', reply_markup=keyboard)
 
 
 
-# Обработчик команды '/start' и '/hello'
+#command /start, /hello handler
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+    bot.reply_to(message, f'Hello im am {bot.get_me().first_name}!')
 
-# Обработчик команды '/heh'
+# command /heh handler , laughing one)
 @bot.message_handler(commands=['heh'])
 def send_heh(message):
     count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
@@ -32,13 +32,13 @@ def create_poll(message):
     )
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text == 'кнопка 1':
-        bot.send_message(message.chat.id, 'вы нажали кнопку 1')
-    elif message.text == 'кнопка 2':
-        bot.send_message(message.chat.id, 'вы нажали кнопку 2')
+    if message.text == 'button 1':
+        bot.send_message(message.chat.id, 'Button 1 is pressed')
+    elif message.text == 'button 2':
+        bot.send_message(message.chat.id, 'Button 1 is pressed')
     elif message.text == 'sup':
-        bot.send_message(message.chat.id, 'вы нажали кнопку 2')
+        bot.send_message(message.chat.id, 'Hello , how are you?')
     elif message.text == 'what are you?':
-        bot.send_message(message.chat.id, 'SUp')
+        bot.send_message(message.chat.id, 'I am testing bot ')
 
 bot.polling()
